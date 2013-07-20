3c8de0a18a9bf061a0fc2e4dcebf9d321f336e78
Eric Stolten <stoltene2@gmail.com>
2013-07-20 20:48:11 +0200

    Initial commit of the blog

---
+++ posts/2013-20-07-why-i-chose-foundation4.markdown
@@ -0,0 +1,6 @@
+---
+title: Why I Chose Foundation 4
+author: Eric Stolten
+---
+
+Whaaat
---
+++ site.hs
@@ -0,0 +1,81 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+
+--------------------------------------------------------------------------------
+-- Assets
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "js/**" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+--------------------------------------------------------------------------------
+-- Individual blog posts
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+-- TODO: what does this do?
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            let indexCtx =
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+--------------------------------------------------------------------------------
+
+publishedPosts :: [Item a] -> m [Item a]
+publishedPosts items = undefined
+               -- return $ filter (\item -> currentDate > metaData date item) items
